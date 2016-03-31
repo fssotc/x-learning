9fb93b244dd8d10cb555fa175d58778ca6bac752
drinetn <drinodroid@gmail.com>
2016-03-31 18:33:23 +0100

    adding semestre matiere and module

--- db_fss/admin.py
+++ db_fss/admin.py
@@ -6,3 +6,8 @@ from .models import Student, Prof, Inscription, Semestre, Module, Matiere
 admin.site.register(Student)
 admin.site.register(Prof)
 admin.site.register(Inscription)
+admin.site.register(Matiere)
+admin.site.register(Module)
+admin.site.register(Semestre)
+
+
