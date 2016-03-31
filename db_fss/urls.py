eed694fe1bea51a11b1d8e4111fecb6640d2b935
DrineTN <drinetn@outlook.com>
2016-03-31 18:56:37 +0100

    home url

--- db_fss/urls.py
+++ db_fss/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from .views import StudentList, StudentDetail,ProfList
 
 urlpatterns = [
+    url(r'^$',home,name='home'),
     url(r'^student/$', StudentList.as_view(), name='student_list'),
     url(r'^student/(?P<pk>[0-9]+)$', StudentDetail.as_view(), name='student_detail'),
     url(r'^prof/$', ProfList.as_view(), name='prof_list'),
